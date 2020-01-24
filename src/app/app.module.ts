9076cfeb30b46254cae3a0beb7b0f26b35a61ad2
Cristian Ruiz <cristianruiz@MacBook-Pro.local>
2020-01-23 19:22:48 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotpagefoundComponent } from './share/notpagefound/notpagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafico1Component } from './pages/grafico1/grafico1.component';
+import { HeaderComponent } from './share/header/header.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './share/breadcrums/breadcrums.component';
+import { APP_ROUTES } from './app.routes';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotpagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Grafico1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
